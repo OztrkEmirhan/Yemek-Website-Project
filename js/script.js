08b334203fdfbe1f26fa28d5c6a8fe7739dabb24
OztrkEmirhan <107921700+OztrkEmirhan@users.noreply.github.com>
2023-07-04 14:21:52 +0300

    branch
    branch

---
+++ js/script.js
@@ -0,0 +1,47 @@
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+
+yearEl.textContent = currentYear;
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    // Scroll back to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+
+    // Close the mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 720) {
+    this.document.body.classList.add("sticky");
+  } else {
+    this.document.body.classList.remove("sticky");
+  }
+});
